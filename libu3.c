80d848705c9c676809456718990adf35b99da844
carolinewambuimutugi <90791513+carolinewambuimutugi@users.noreply.github.com>
2021-10-22 14:56:34 +0300

    Add files via upload
    library mangement project

---
+++ libu3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("zetech library\n");
+    printf("Welcome caroline\n");
+    printf("what would you lke to do\n");
+    printf("1.add a new user\n");
+    printf(".get a new book\n");
+    return 0;
+}
